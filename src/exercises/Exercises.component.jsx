f7068c62d43378542c5c29f789dfcfc3b268aed0
Mar <hammi.marouane@gmail.com>
2018-09-09 21:50:53 +0200

    forgot files

---
+++ src/exercises/Exercises.component.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const exos = [
+    {
+        url: "https://codesandbox.io/embed/new?codemirror=1",
+        explanation: () => (<div>here what you have to do</div>)
+    },
+    {
+        url: "https://codesandbox.io/embed/new?codemirror=2",
+        explanation: () => (<div>here what you have not to do for this one</div>)
+    },
+    {
+        url: "https://codesandbox.io/embed/new?codemirror=3",
+        explanation: () => (<div>here you are</div>)
+    },
+];
+
+export default exos;
