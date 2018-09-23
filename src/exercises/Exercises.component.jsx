f3ae0e4d63fcac689f6e907e2c6a57fc93cd43e9
Issam Hammi <issamhammi@Issams-MacBook-Air.local>
2018-09-23 15:38:57 +0200

    exo2 enhanced

--- src/exercises/Exercises.component.jsx
+++ src/exercises/Exercises.component.jsx
@@ -87,8 +87,8 @@ const exos = [
           </li>
         </ul>
         <p>
-          YOUR TURN !!! First, let's create some variables and bind to them
-          values.
+          <strong>YOUR TURN !!!</strong> First, let's create some variables and
+          bind to them values.
         </p>
         <ul>
           <li>
@@ -114,19 +114,39 @@ const exos = [
       <div className="content">
         <h1>A bit of maths</h1>
         <p>
-          Pikachu wants to use thunder on the evil Miaouss. Thunder is a
-          powerful attack with a power of 120 !!! Using the folowing formula{" "}
-          <code> Damage = Power * Attack / Defense</code> Calculate the damage
+          The main thing to do with numbers is arithmetic. Arithmetic operations
+          such as addition or multiplication take two number values and produce
+          a new number from them. Here is what they look like in JavaScript:{" "}
+          <code>100+4*11</code>
+        </p>
+        <p>
+          The <strong>+</strong> and <strong>*</strong> symbols are called{" "}
+          <strong>operators</strong>. The first stands for addition, and the
+          second stands for multiplication. Putting an operator between two
+          values will apply it to those values and produce a new value. But does
+          the example mean “add 4 and 100, and multiply the result by 11,” or is
+          the multiplication done before the adding? As you might have guessed,
+          the multiplication happens first. But as in mathematics, you can
+          change this by wrapping the addition in parentheses:{" "}
+          <code>(100+4)*11</code>
+        </p>
+        <p>
+          For subtraction, there is the <strong>-</strong> operator, and
+          division can be done with the <strong>/</strong> operator.
+        </p>
+        <p>
+          <strong>YOUR TURN !!!</strong> Pikachu wants to use thunder on the
+          evil Miaouss. Thunder is a powerful attack with a power of 120 !!!
+          Using the folowing formula{" "}
+          <code> damage = power * attack / defense</code> Calculate the damage
           done on the Miaouss.
         </p>
-        <div>
-          <ul>
-            <li>
-              Use arithmetic operations to compute the result and bind it to the
-              variable <code>damage</code>
-            </li>
-          </ul>
-        </div>
+        <ul>
+          <li>
+            Use arithmetic operations to compute the result and bind it to the
+            variable <code>damage</code>
+          </li>
+        </ul>
       </div>
     )
   },
