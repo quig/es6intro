0715a4ca1aa90dc19028cd5afb6f81af21a56582
Issam Hammi <issamhammi@Issams-MacBook-Air.local>
2018-09-30 22:22:03 +0200

    improves content on classes

--- src/exercises/Exercises.component.jsx
+++ src/exercises/Exercises.component.jsx
@@ -4,6 +4,7 @@ import strings from "../assets/strings.png";
 import objects from "../assets/objects.png";
 import arrays from "../assets/arrays.png";
 import literals from "../assets/template-literals.png";
+import classes from "../assets/classes.png";
 
 const exos = [
   {
@@ -672,16 +673,44 @@ const exos = [
         </p>
         <p>
           {" "}
-          A class defines the shape of a type of object—what methods and
-          properties it has. Such an object is called an instance of the class.
+          A <strong>class</strong> defines the shape of a type of object—what
+          methods and properties it has. Such an object is called an instance of
+          the class.
         </p>
         <p>
-          <strong>YOUR TURN !!!</strong>
+          Imagine having a farm of rabbits, each with its own specificities
+          (different name or color for example). You can create a class Rabbit
+          representing the definition of what is a rabbit, then instanciate
+          rabbits depending on your needs.
+        </p>
+        <img src={classes} alt="Rabbits" />
+        <p>
+          The <code>class</code> keyword starts a class declaration, which
+          allows us to define a constructor and a set of methods all in a single
+          place. Any number of methods may be written inside the declaration’s
+          braces. The one named <strong>constructor</strong> is treated
+          specially. It provides the actual constructor function, which will be
+          bound to the name Rabbit. The <code>new</code> keyword allows us to
+          call the constructor of the class.
+        </p>
+        <p className="is-italic">
+          the binding called <code>this</code> in the constructor automatically
+          points at the object that it was called on.
+        </p>
+        <p>
+          A class definition can inherit from another class which means share
+          its properties and methods. Although it is often used in React, we
+          will not dig into inheritence for now.
+        </p>
+        <p>
+          <strong>YOUR TURN !!!</strong> Pikachu and Miaouss are instances of
+          the class Pokemon.
         </p>
         <ul>
           <li>
             Create a class <code>Pokemon</code> with the following properties:{" "}
-            <code>attack</code>
+            <code>name</code>, <code>attack</code>, <code>defense</code>,{" "}
+            <code>speed</code> and <code>moves</code>
           </li>
         </ul>
       </div>
